de930dd5c3d79111cb62955322eb13dc6d8737ef
LoskoMiguel <magelposada@gmail.com>
2025-04-04 19:43:43 -0500

    se crea estructura de carpetas

---
+++ routes/admin/registrar.py
@@ -0,0 +1,6 @@
+from db.conexion import get_conection
+
+conn = get_conection()
+cursor = conn.cursor()
+
+#hacer sistema de registro
---
+++ routes/login.py
@@ -0,0 +1,4 @@
+from db.conexion import get_conection
+
+conn = get_conection()
+cursor = conn.cursor()
