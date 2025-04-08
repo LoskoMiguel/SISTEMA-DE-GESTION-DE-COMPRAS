ccd0a05db6e5d3ecc78830bfe99e3a1e057337b7
andresxxz102 <chaparroluigy102@gmail.com>
2025-04-07 21:47:26 -0500

    Se agrego, añadir producto y eliminar producto

--- routes/admin/productos/agregar_producto.py
+++ routes/admin/productos/agregar_producto.py
@@ -2,3 +2,32 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 from db.conexion import get_conection
+
+def agregar_producto():
+    conn = get_conection()
+    cursor = conn.cursor()
+
+    nombre = input("Nombre del producto: ")
+
+    cursor.execute("SELECT producto, stock ,price FROM productos WHERE producto = ?", (nombre,))
+    producto = cursor.fetchone()
+    
+    if producto:
+        print ("❌ El producto ya existe en la base de datos.")
+        return
+    else:
+        print("✅ Producto no encontrado, puede agregarlo.")
+        nombre = input("Ingrese el nombre del producto: ")
+        stock = int(input("Ingrese el stock del producto: "))
+        price = float(input("Ingrese el precio del producto: "))
+
+        # Insertar el nuevo producto en la base de datos
+        cursor.execute("INSERT INTO productos (producto, stock, price) VALUES (?, ?, ?)", (nombre, stock, price))
+        conn.commit()
+        print("✅ Producto agregado correctamente.")
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    agregar_producto()
--- routes/admin/productos/eliminar_producto.py
+++ routes/admin/productos/eliminar_producto.py
@@ -2,3 +2,31 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 from db.conexion import get_conection
+
+def eliminar_producto():
+    conn = get_conection()
+    cursor = conn.cursor()
+
+    nombre = input("Ingrese el Nombre del producto que desea eliminar: ")
+
+    cursor.execute("SELECT producto, stock ,price FROM productos WHERE producto = ?", (nombre,))
+    producto = cursor.fetchone()
+
+    if producto:
+        print("✅ Producto encontrado.")
+
+        opcion = input("""opción:
+            1. Eliminar Producto  """)
+
+        if opcion == "1":
+            cursor.execute("DELETE FROM productos WHERE producto = ?", (nombre,))
+            conn.commit()
+            print("✅ Producto eliminado con éxito.")
+
+    else:
+        print("❌ Producto no encontrado.")
+        return
+
+
+if __name__ == "__main__":
+    eliminar_producto()
